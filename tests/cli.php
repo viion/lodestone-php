5db91ea9c6011b88a4dedfcaead212d860b913d1
Josh Freeman <Josh@viion.co.uk>
2017-04-18 22:27:35 +0100

    Improve the CLI test script to allow multiple tests

--- tests/cli.php
+++ tests/cli.php
@@ -3,11 +3,36 @@
 // composer auto loader
 require __DIR__.'/../vendor/autoload.php';
 
+define('LOGGER_ENABLED', true);
+
 // parse characters
 // view Lodestone/Modules/Routes for more urls.
-// todo: write a simple route wrapper.
-$url = sprintf(\Lodestone\Modules\Routes::LODESTONE_CHARACTERS_URL, 16412004);
-$parser = new Lodestone\Parser\Character();
+
+$option = isset($argv[1]) ? trim($argv[1]) : false;
+$id = isset($argv[2]) ? trim($argv[2]) : false;
+if (!$option) {
+    die('Please provide an option: character, fc, ls');
+}
+
+// load parser from command arg
+switch($option) {
+    case 'character':
+        $url = sprintf(\Lodestone\Modules\Routes::LODESTONE_CHARACTERS_URL, $id ? $id : 730968);
+        $parser = new Lodestone\Parser\Character();
+        break;
+
+    case 'fc':
+        $url = sprintf(\Lodestone\Modules\Routes::LODESTONE_FREECOMPANY_URL, $id ? $id : '9231253336202687179');
+        $parser = new Lodestone\Parser\FreeCompany();
+        break;
+
+    case 'ls':
+        $url = sprintf(\Lodestone\Modules\Routes::LODESTONE_LINKSHELL_MEMBERS_URL, $id ? $id : '19984723346535274');
+        $parser = new Lodestone\Parser\Linkshell();
+        break;
+}
+
+// Get data from parser
 $data = $parser->url($url)->parse();
 
 // Array of character data

bf216034d90895bfd4d7c3bd242c1a1acfa2f644
simbiat@bk.ru <simbiat@bk.ru>
2017-05-06 15:14:06 +0300

    False HTML handler
    Added handling of regular false return from HTTPRequest and allowed returning it

--- src/Lodestone/Parser/Character.php
+++ src/Lodestone/Parser/Character.php
@@ -27,8 +27,8 @@ class Character extends ParserHelper
      */
     public function parse($hash = false)
     {
-        if ($this->html == 404) {
-        	return 404;
+        if ($this->html == 404 || ($this->html == false) {
+        	return $this->html;
         }
         $this->ensureHtml();
         $html = $this->html;

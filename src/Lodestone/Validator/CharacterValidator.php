5faad726a2c101d609e677e6508b6ae694515c03
Josh Freeman <josh.freeman@extreme-creations.co.uk>
2017-05-18 13:53:55 +0100

    Implement basic character validation based on @shentschel mockup.

---
+++ src/Lodestone/Validator/CharacterValidator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Lodestone\Validator;
+
+/**
+ * Class CharacterValidator
+ * @package Lodestone\Validator
+ */
+class CharacterValidator extends BaseValidator
+{
+    const VALID_CHARACTER_REGEX = '/^[a-zA-Z\' ]+\s?$/';
+
+    /**
+     * CharacterValidator constructor.
+     * @param $object
+     * @param $name
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * @return $this
+     */
+    public function isValidCharacerName()
+    {
+       if (!preg_match(self::VALID_CHARACTER_REGEX, $this->object)) {
+           $this->errors[] = new ValidationException($this->object . ' is not a valid character name.');
+        }
+
+       return $this;
+    }
+}

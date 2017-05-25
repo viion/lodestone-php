4ed87b9c9b51f43c90d8c83cde99ca967cdf8bcb
Josh Freeman <josh.freeman@extreme-creations.co.uk>
2017-05-25 17:09:37 +0100

    Implement auto toArray method.

--- src/Lodestone/Entities/AbstractEntity.php
+++ src/Lodestone/Entities/AbstractEntity.php
@@ -2,6 +2,7 @@
 
 namespace Lodestone\Entities;
 
+use Lodestone\Modules\Benchmark;
 use Lodestone\Validator\BaseValidator;
 
 /**
@@ -31,5 +32,54 @@ class AbstractEntity
     {
         $this->validator = new BaseValidator();
         return $this;
+    }
+
+    /**
+     * Map all class attributes
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        Benchmark::start(__METHOD__,__LINE__);
+        $reflector = new \ReflectionClass(get_class($this));
+
+        // get properties
+        $properties = $reflector->getProperties(\ReflectionProperty::IS_PUBLIC);
+
+        // loop through properties
+        $arr = [];
+        foreach($properties as $property) {
+            $doc = $reflector->getProperty($property->name)->getDocComment();
+
+            // parse fields
+            $result = [];
+            if (preg_match_all('/@(\w+)\s+(.*)\r?\n/m', $doc, $matches)) {
+                $result = array_combine($matches[1], $matches[2]);
+            }
+
+            // only add those with a var type
+            if (isset($result['var'])) {
+                // get base type
+                switch(explode('|', $result['var'])[0]) {
+                    // basic
+                    case 'string':
+                    case 'array':
+                    case 'int':
+                    case 'bool':
+                    case 'float':
+                        $arr[$property->name] = $this->{$property->name};
+                        break;
+
+                    // assume a class, get its data
+                    default:
+                        $arr[$property->name] = $this->{$property->name}->toArray();
+                        break;
+                }
+            }
+        }
+        Benchmark::finish(__METHOD__,__LINE__);
+
+        return $arr;
     }
 }

052721ed0cec5f9f183ea4204a42d3163cccc71d
Josh Freeman <josh.freeman@extreme-creations.co.uk>
2017-06-08 17:26:17 +0100

    Fix time leak in gear from incorrectly using $box

--- src/Lodestone/Parser/Character/TraitGear.php
+++ src/Lodestone/Parser/Character/TraitGear.php
@@ -17,9 +17,14 @@ trait TraitGear
      */
     protected function parseEquipGear()
     {
+        $a = round(microtime(true) * 1000);
+
+        print_r("\n");
         Benchmark::start(__METHOD__,__LINE__);
+
         $box = $this->getSpecial__EquipGear();
 
+        // loop
         foreach($box->find('.item_detail_box') as $i => $node) {
             $item = new Item();
 
@@ -33,67 +38,74 @@ trait TraitGear
                 ->setLodestoneId($lodestoneId)
                 ->setId($id);
 
-            // category
-            $category = $box->find('.db-tooltip__item__category', 0)->plaintext;
-            $category = explode("'", $category)[0];
-            $category = str_ireplace(['Two-handed', 'One-handed'], null, $category);
-            $item->setCategory(trim($category));
+            // category - only important on first one as it's used
+            // to determine active class
+            if ($i == 0) {
+                $category = $node->find('.db-tooltip__item__category')->plaintext;
+                $category = explode("'", $category)[0];
+                $category = str_ireplace(['Two-handed', 'One-handed'], null, $category);
+                $item->setCategory(trim($category));
+            }
+
+
+
+
+            /*
 
             // add mirage
             $mirageNode = $node->find('.db-tooltip__item__mirage');
             if ($mirageNode) {
-                $mirageNode = $mirageNode->find('a', 0);
-                if ($mirageNode) {
-                    $mirageId = explode('/', $mirageNode->getAttribute('href'))[5];
-                    $item->setMirageId($mirageId);
-                    // todo - parse name and convert to ingame id
+               $mirageNode = $mirageNode->find('a', 0);
+               if ($mirageNode) {
+                   $mirageId = explode('/', $mirageNode->getAttribute('href'))[5];
+                   $item->setMirageId($mirageId);
+                   // todo - parse name and convert to ingame id
 
-                }
+               }
             }
-
 
             // add creator
             $creatorNode = $node->find('.db-tooltip__signature-character');
             if ($creatorNode) {
-                $creatorNode = $creatorNode->find('a',0);
-                if ($creatorNode) {
-                    $creatorId = explode('/', $creatorNode->getAttribute('href'))[3];
-                    $item->setCreatorId($creatorId);
-                }
+               $creatorNode = $creatorNode->find('a',0);
+               if ($creatorNode) {
+                   $creatorId = explode('/', $creatorNode->getAttribute('href'))[3];
+                   $item->setCreatorId($creatorId);
+               }
             }
-
 
             // add dye
             $dyeNode = $node->find('.stain');
             if ($dyeNode) {
-                $dyeNode = $dyeNode->find('a',0);
-                if ($dyeNode) {
-                    $dyeId = explode('/', $dyeNode->getAttribute('href'))[5];
-                    $item->setDyeId($dyeId);
-                    // todo - parse name and convert to ingame id
-                }
+               $dyeNode = $dyeNode->find('a',0);
+               if ($dyeNode) {
+                   $dyeId = explode('/', $dyeNode->getAttribute('href'))[5];
+                   $item->setDyeId($dyeId);
+                   // todo - parse name and convert to ingame id
+               }
             }
-
 
             // add materia
             $materiaNodes = $node->find('.db-tooltip__materia',0);
             if ($materiaNodes) {
-                if ($materiaNodes = $materiaNodes->find('li')) {
-                    foreach ($materiaNodes as $mnode) {
-                        $mhtml = $mnode->find('.db-tooltip__materia__txt')->innerHtml();
-                        if (!$mhtml) {
-                            continue;
-                        }
+               if ($materiaNodes = $materiaNodes->find('li')) {
+                   foreach ($materiaNodes as $mnode) {
+                       $mhtml = $mnode->find('.db-tooltip__materia__txt')->innerHtml();
+                       if (!$mhtml) {
+                           continue;
+                       }
 
-                        list($mname, $mvalue) = explode('<br>', html_entity_decode($mhtml));
+                       list($mname, $mvalue) = explode('<br>', html_entity_decode($mhtml));
 
-                        $item->addMateria([
-                            'name' => trim(strip_tags($mname)),
-                            'value' => trim(strip_tags($mvalue)),
-                        ]);
-                    }
-                }
+                       $item->addMateria([
+                           'name' => trim(strip_tags($mname)),
+                           'value' => trim(strip_tags($mvalue)),
+                       ]);
+                   }
+               }
             }
+
+            */
 
             // slot conditions, based on category
             $slot = $node->find('.db-tooltip__item__category', 0)->plaintext;
@@ -119,5 +131,11 @@ trait TraitGear
 
         unset($box);
         Benchmark::finish(__METHOD__,__LINE__);
+
+        $b = round(microtime(true) * 1000) - $a;
+        file_put_contents(__DIR__.'/gear.log', $b . "\n", FILE_APPEND);
+
+        print_r('X = '. $b . "\n");
+        print_r("\n");
     }
 }

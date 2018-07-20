9eb8ab091b099d81be266ecb87596f968e93fdf9
Simbiat <simbiat@bk.ru>
2018-07-20 16:43:25 +0300

    Free Company search regex
    Using regex to parse free company search results

--- Lodestone/Search/FreeCompany.php
+++ Lodestone/Search/FreeCompany.php
@@ -2,17 +2,12 @@
 
 namespace Lodestone\Search;
 
-use Lodestone\Entities\{
-    FreeCompany\FreeCompanySimple
-};
-use Lodestone\Parser\Html\ParserHelper;
-
 /**
  * Class Search
  *
- * @package Lodestone\PSearchFreeCompany
+ * @package Lodestone\SearchFreeCompany
  */
-class FreeCompany extends ParserHelper
+class FreeCompany
 {
     /**
      * Parser constructor.
@@ -21,43 +16,38 @@ class FreeCompany extends ParserHelper
      */
     function __construct(string $html)
     {
-        $this->results = new SearchFreeCompany();
-        $this->html = $html;
-        $this->initialize();
-        
-        if (!$this->getDocument()->find('.parts__zero', 0)) {
-            $this->pageCount();
-            $this->parseList();
-        }
-    }
-    
-    /**
-     * Parse members
-     */
-    private function parseList()
-    {
-        if ($this->results->getTotal() == 0) {
-            return;
-        }
-        
-        $rows = $this->getDocumentFromClassname('.ldst__window');
-        
-        // loop through the list of characters
-        foreach($rows->find('.entry') as $node) {
-            // get all emblum imgs
-            $crest = [];
-            foreach($node->find('.entry__freecompany__crest__image img') as $img) {
-                $crest[] = explode('?', $img->src)[0];
+        preg_match_all(
+            '/<div class="parts__total">(?<total>\d*).*<\/div>.*<li class="btn__pager__current">(Page |Seite )*(?<pageCurrent>\d*)[^\d]*(?<pageTotal>\d*).*<\/li>/mis',
+            $html,
+            $pages,
+            PREG_SET_ORDER
+        );
+        $this->results = [
+            'pageCurrent'=>$pages[0]['pageCurrent'],
+            'pageTotal'=>$pages[0]['pageTotal'],
+            'total'=>$pages[0]['total'],
+        ];
+        preg_match_all(
+            '/<div class="entry"><a href="\/lodestone\/freecompany\/(?<fcid>\d*)\/" class="entry__block"><div class="entry__freecompany__inner"><div class="entry__freecompany__crest"><div class="entry__freecompany__crest--position"><img src=".*" width="68" height="68" alt="" class="entry__freecompany__crest__base"><div class="entry__freecompany__crest__image"><img src="(?<fccrestimg1>https:.{58,74}\.png)" width="64" height="64"( alt=".*")?>(<img src="(?<fccrestimg2>https:.{58,74}\.png)" width="64" height="64"( alt=".*")?>)?(<img src="(?<fccrestimg3>https:.{58,74}\.png)" width="64" height="64"( alt=".*")?>)?<\/div><\/div><\/div><div class="entry__freecompany__box"><p class="entry__world">(?<gcname>.*)<\/p><p class="entry__name">(?<name>.*)<\/p><p class="entry__world">(?<server>.*)<\/p><\/div><div class="entry__freecompany__data">.*<\/div><\/div><ul class="entry__freecompany__fc-data clearix"><li class="entry__freecompany__fc-member">(?<members>\d*)<\/li><li class="entry__freecompany__fc-housing">(?<housing>.*)<\/li><li class="entry__freecompany__fc-day"><span id="datetime-.*">.*<\/span><script>document\.getElementById\(\'datetime-.*\'\)\.innerHTML = ldst_strftime\((?<found>.*), \'YMD\'\);<\/script><\/li><li class="entry__freecompany__fc-active">.*: (?<active>.*)<\/li><li class="entry__freecompany__fc-active">.*: (?<recruitment>.*)<\/li><\/ul><\/a><\/div>/mi',
+            $html,
+            $freecompanies,
+            PREG_SET_ORDER
+        );
+        foreach ($freecompanies as $key=>$freecompany) {
+            foreach ($freecompany as $key2=>$details) {
+                if (is_numeric($key2) || empty($details)) {
+                    unset($freecompanies[$key][$key2]);
+                }
             }
-            
-            // create simple free company
-            $obj = new FreeCompanySimple();
-            $obj->setId( explode('/', $node->find('a', 0)->getAttribute('href'))[3] )
-                ->setName( trim($node->find('.entry__name')->plaintext) )
-                ->setServer( trim($node->find('.entry__world', 1)->plaintext) )
-                ->setAvatar( $crest );
-            
-            $this->results->addFreeCompany($obj);
+            $freecompanies[$key]['crest'][] = str_replace('64x64', '128x128', $freecompany['fccrestimg1']);
+            if (!empty($freecompany['fccrestimg2'])) {
+                $freecompanies[$key]['crest'][] = str_replace('64x64', '128x128', $freecompany['fccrestimg2']);
+            }
+            if (!empty($freecompany['fccrestimg3'])) {
+                $freecompanies[$key]['crest'][] = str_replace('64x64', '128x128', $freecompany['fccrestimg3']);
+            }
+            unset($freecompanies[$key]['fccrestimg1'], $freecompanies[$key]['fccrestimg2'], $freecompanies[$key]['fccrestimg3']);
         }
+        $this->results['freeCompanies'] = $freecompanies;
     }
 }

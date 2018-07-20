c0d7c1969f9139116dd27b39eaee9f4d1c3f563f
Simbiat <simbiat@bk.ru>
2018-07-20 15:34:53 +0300

    Character Search Regex
    Now uses regex instead of full HTML parsing

--- Lodestone/Search/Character.php
+++ Lodestone/Search/Character.php
@@ -2,32 +2,63 @@
 
 namespace Lodestone\Search;
 
-use Lodestone\Entities\{
-    Character\CharacterSimple
-};
-use Lodestone\Parser\Html\ParserHelper;
-
 /**
  * Class Search
  *
  * @package Lodestone\Parser\Character
  */
-class Character extends ParserHelper
+class Character
 {
-    /**
-     * Parser constructor.
-     *
-     * @param int $id
-     */
+    public $results;
+    
     function __construct(string $html)
     {
-        $this->results = new SearchCharacter();
-        $this->html = $html;
-        $this->initialize();
-
-        if (!$this->getDocument()->find('.parts__zero', 0)) {
-            $this->pageCount();
-            $this->parseCharacterList();
+        preg_match_all(
+            '/<div class="parts__total">(?<total>\d*).*<\/div>.*<li class="btn__pager__current">(Page |Seite )*(?<pageCurrent>\d*)[^\d]*(?<pageTotal>\d*).*<\/li>/mis',
+            $html,
+            $pages,
+            PREG_SET_ORDER
+        );
+        $this->results = [
+            'pageCurrent'=>$pages[0]['pageCurrent'],
+            'pageTotal'=>$pages[0]['pageTotal'],
+            'total'=>$pages[0]['total'],
+        ];
+        preg_match_all(
+            '/<div class="entry"><a href="\/lodestone\/character\/(?<id>\d*)\/" class="entry__link"><div class="entry__chara__face"><img src="(?<avatar>https:.*\.jpg).*" alt=".*"><\/div><div class="entry__box entry__box--world"><p class="entry__name">(?<name>.*)<\/p><p class="entry__world">(?<server>.*)<\/p><ul class="entry__chara_info"><li><i class="list__ic__class"><img src=".*" width="20" height="20" alt=""><\/i><span>.*<\/span><\/li>(<li class="js__tooltip" data-tooltip="(?<gcname>.*) \/ (?<gcrank>.*)"><img src="(?<gcrankicon>https:.*\.png)" width="20" height="20" alt=""><\/li>){0,1}?<\/ul><\/div><div class="entry__chara__lang">(?<language>.*)<\/div><\/a>(<a href="\/lodestone\/freecompany\/(?<fcid>\d*)\/" class="entry__freecompany__link"><i class="list__ic__crest"><img src="(?<fccrestimg1>https:.{74}\.png)" width="18" height="18">(<img src="(?<fccrestimg2>https:.{74}\.png)" width="18" height="18">){0,1}(<img src="(?<fccrestimg3>https:.{74}\.png)" width="18" height="18">){0,1}<\/i><span>(?<fcname>.*)<\/span><\/a>){0,1}<\/div>/mi',
+            $html,
+            $characters,
+            PREG_SET_ORDER
+        );
+        foreach ($characters as $key=>$character) {
+            foreach ($character as $key2=>$details) {
+                if (is_numeric($key2) || empty($details)) {
+                    unset($characters[$key][$key2]);
+                }
+            }
+            if (!empty($character['gcname'])) {
+                $characters[$key]['grandCompany'] = [
+                    'name'=>$character['gcname'],
+                    'rank'=>$character['gcrank'],
+                    'icon'=>$character['gcrankicon'],
+                ];
+            }
+            if (!empty($character['fcid'])) {
+                $characters[$key]['freeCompany'] = [
+                    'id'=>$character['fcid'],
+                    'name'=>$character['fcname'],
+                    'crest'=>[],
+                ];
+                $characters[$key]['freeCompany']['crest'][] = $character['fccrestimg1'];
+                if (!empty($character['fccrestimg2'])) {
+                    $characters[$key]['freeCompany']['crest'][] = $character['fccrestimg2'];
+                }
+                if (!empty($character['fccrestimg3'])) {
+                    $characters[$key]['freeCompany']['crest'][] = $character['fccrestimg3'];
+                }
+            }
+            unset($characters[$key]['gcname'], $characters[$key]['gcrank'], $characters[$key]['gcrankicon'], $characters[$key]['fcid'], $characters[$key]['fcname'], $characters[$key]['fccrestimg1'], $characters[$key]['fccrestimg2'], $characters[$key]['fccrestimg3']);
         }
+        $this->results['characters'] = $characters;
     }
 }
--- README.md
+++ README.md
@@ -9,9 +9,11 @@ The goal is to provide an extremely fast and lightweight library, it is built wi
 
 ## What's different?
 If you are using the original library there is not a "need" to switch, but this update already has some differences, that may get you interested:
-    1. It has slightly different code structure, that aims at reduction of rarely used or unnecessary functions and some standartization (on-going attempt). Tests show that in some cases it can provide slightly faster results winning ~10ms of time. If you are using it heavily like I am doing on [FFTracker](https://simbiat.ru/fftracker) you will be greatful even for such small boost. This also implies some code cleaning and, potentially, retirement of objects in some cases to get additional boost.
-    2. More filters for your queries. This is mainly for search subfunctions, but other calls also may already or will have additional options to provide more flexibility.
-    3. Attempt at multilangual support. Search functions already support multilingual filters (insterad of IDs) and at the moment, it looks like only some character parser functions need to be updated to provide full support.
+    1. It has different code structure, that aims at reduction of rarely used or unnecessary functions and some standartization (on-going attempt). Tests show that in some cases it can provide slightly faster results winning ~10ms of time. If you are using it heavily like I am doing on [FFTracker](https://simbiat.ru/fftracker) you will be greatful even for such small boost. This also implies some code cleaning and, potentially, retirement of objects in some cases to get additional boost.
+    2. Using regex instead of full HTML parsing for extra speed, where possible.
+    3. More filters for your queries. This is mainly for search subfunctions, but other calls also may already or will have additional options to provide more flexibility.
+    4. Return more potential useful information in searches.
+    5. Attempt at multilangual support. Search functions already support multilingual filters (insterad of IDs) and at the moment, it looks like only some character parser functions need to be updated to provide full support.
 
 
 ## Getting Started
